f2534bf039d519c3faf95b43f3997fb15310ee2d
Jakob Primdal <jakobprimdal@gmail.com>
2026-02-25 15:22:16 +0100

    Implemented CrossOrigin for frontend

--- src/main/java/dk/cvr/backend/controller/CompanyController.java
+++ src/main/java/dk/cvr/backend/controller/CompanyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 @RequestMapping("/api/company")
 public class CompanyController {
