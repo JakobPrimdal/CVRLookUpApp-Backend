2350c6f0e08b09c52cb0ce9fd5ff6848413f9391
Jakob Primdal <jakobprimdal@gmail.com>
2026-03-03 16:28:31 +0100

    Removed: Debuging in controller

--- src/main/java/dk/cvr/backend/controller/CompanyController.java
+++ src/main/java/dk/cvr/backend/controller/CompanyController.java
@@ -24,7 +24,6 @@ public class CompanyController {
 
     @GetMapping("/{cvr}")
     public CompanyResponseDTO getCompany(@PathVariable String cvr) {
-        throw new CompanyNotFoundException("Test Exception");
-        //return manager.getCvrByNumber(cvr);
+        return manager.getCvrByNumber(cvr);
     }
 }
